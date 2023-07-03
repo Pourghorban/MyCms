e5dd28aa5adf1e6058223141767def9e16e4b279
MR.pourghorban <m.pourghorban2004@gmail.com>
2023-07-03 17:34:07 +0330

    initial databaase

---
+++ DataLayer/Context/MyCmsContext.cs
@@ -0,0 +1,31 @@
+﻿using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace Mitra_Karimi.Context
+{
+    public class MyCmsContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public MyCmsContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        public DbSet<PageGroup> PageGroups { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<PageComment> PageComments { get; set; }
+        public DbSet<AdminLogin> AdminLogins { get; set; }
+    }
+}
