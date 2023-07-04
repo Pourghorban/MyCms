297faedac8195cce9c837dc2011d9c517a047119
MR.pourghorban <m.pourghorban2004@gmail.com>
2023-07-04 03:40:29 +0330

    aa

--- Mitra Karimi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mitra_Karimi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
