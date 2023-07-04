e8f2085144ba02f3969e849f7c13b233414766e9
MR.pourghorban <m.pourghorban2004@gmail.com>
2023-07-04 03:44:23 +0330

    aaaa

---
+++ CMS_MitraKarimi/CMS/Areas/Admin/Controllers/PageGroupsController.cs
@@ -0,0 +1,126 @@
+﻿using System.Net;
+using DataLayer;
+using System.Web.Mvc;
+
+namespace MyCms.Areas.Admin.Controllers
+{
+
+    public class PageGroupsController : Controller
+    {
+        private IPageGroupRepository pageGroupRepository;
+        public PageGroupsController(IPageGroupRepository pageGroupRepository)
+        {
+            this.pageGroupRepository = pageGroupRepository;
+        }
+
+        // GET: Admin/PageGroups
+        public ActionResult Index()
+        {
+            return View(pageGroupRepository.GetAllGroups());
+        }
+
+      //  [AllowAnonymous]
+        // GET: Admin/PageGroups/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = pageGroupRepository.GetGroupById(id.Value);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        // POST: Admin/PageGroups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GroupID,GroupTitle")] PageGroup pageGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                pageGroupRepository.InsertGroup(pageGroup);
+                pageGroupRepository.save();
+                return RedirectToAction("Index");
+            }
+
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = pageGroupRepository.GetGroupById(id.Value);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(pageGroup);
+        }
+
+        // POST: Admin/PageGroups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GroupID,GroupTitle")] PageGroup pageGroup)
+        {
+            if (ModelState.IsValid)
+            {
+                pageGroupRepository.UpdateGroup(pageGroup);
+                pageGroupRepository.save();
+                return RedirectToAction("Index");
+            }
+            return View(pageGroup);
+        }
+
+        // GET: Admin/PageGroups/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PageGroup pageGroup = pageGroupRepository.GetGroupById(id.Value);
+            if (pageGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(pageGroup);
+        }
+
+        // POST: Admin/PageGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            pageGroupRepository.DeleteGroup(id);
+            pageGroupRepository.save();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                pageGroupRepository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
